b75ff1ff985d643dc5bfad5e6f1915595307afc2
sonbankclerk <131222450+sonbankclerk@users.noreply.github.com>
2023-11-09 03:49:55 +0900

    [level unrated] Title: 편지, Time: 0.02 ms, Memory: 74 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120898. 편지/편지.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(String message) {
+        int answer = 0;
+        int i=0;
+        while(i < message.length()){
+            i++;
+        }
+        
+        return answer=i*2;
+    }
+}
