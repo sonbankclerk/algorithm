0b775f25b8786cf362e08708ba34375c5744f8d9
sonbankclerk <131222450+sonbankclerk@users.noreply.github.com>
2023-11-22 00:01:55 +0900

    [level unrated] Title: 주사위 게임 1, Time: 0.01 ms, Memory: 75.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181839. 주사위 게임 1/주사위 게임 1.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(int a, int b) {
+        int answer = 0;
+        
+        if(a%2!=0 && b%2!=0){
+            answer = (a*a)+(b*b);
+        }else if(a%2!=0 || b%2!=0){
+            answer = 2*(a+b);
+        }else if(b>a){
+            answer = (a-b)*-1;
+        }else{
+            answer = a-b;
+        }
+        
+        return answer;
+    }
+}
