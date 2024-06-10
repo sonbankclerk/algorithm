b87a5ab870480af6c08d2ae4638a2906e4191518
sonbankclerk <thsgoddnjs@gmail.com>
2024-06-11 00:09:50 +0900

    [level 1] Title: 과일 장수, Time: 0.36 ms, Memory: 73.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int k, int m, int[] score) {
+        int answer = 0;
+        
+        Arrays.sort(score);
+        
+        int n = score.length;
+        for(int i = n - m; i >= 0; i -= m) {
+            answer += score[i] * m;
+        }
+        
+        return answer;
+    }
+}
