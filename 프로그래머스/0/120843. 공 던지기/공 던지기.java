d4ffceab10071df5cb32ab52ec9e5d86cc8c15f7
sonbankclerk <thsgoddnjs@gmail.com>
2024-11-14 14:55:15 +0900

    [level 0] Title: 공 던지기, Time: 0.04 ms, Memory: 77.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120843. 공 던지기/공 던지기.java
@@ -0,0 +1,7 @@
+class Solution {
+    public int solution(int[] numbers, int k) {
+        int answer = 0;
+        int index = (k-1)*2% numbers.length;
+        return answer=numbers[index];
+    }
+}
