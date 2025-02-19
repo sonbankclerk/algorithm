48e31f05d67d26f79792e601acc9e0ff39a8e06b
sonbankclerk <thsgoddnjs@gmail.com>
2025-02-19 16:23:04 +0900

    [level 1] Title: 어린 동물 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/59037. 어린 동물 찾기/어린 동물 찾기.sql
+++ 프로그래머스/1/59037. 어린 동물 찾기/어린 동물 찾기.sql
@@ -1,5 +1,5 @@
 -- 코드를 입력하세요
-SELECT ANIMAL_ID, NAME
-FROM ANIMAL_INS
-WHERE INTAKE_CONDITION NOT IN 'Aged'
-ORDER BY ANIMAL_ID
+SELECT animal_id, name
+FROM animal_ins
+WHERE intake_condition != 'Aged'
+ORDER BY animal_id
