0f4b5325a8370169cbd1aa3c7e0ff09258896be2
sonbankclerk <thsgoddnjs@gmail.com>
2025-02-21 03:14:16 +0900

    [level 4] Title: 오프라인/온라인 판매 데이터 통합하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/131537. 오프라인／온라인 판매 데이터 통합하기/오프라인／온라인 판매 데이터 통합하기.sql
+++ 프로그래머스/4/131537. 오프라인／온라인 판매 데이터 통합하기/오프라인／온라인 판매 데이터 통합하기.sql
@@ -1,5 +1,6 @@
 -- 코드를 입력하세요
-SELECT TO_CHAR(A.SALES_DATE, 'YYYY-MM-DD'), A.PRODUCT_ID, A.USER_ID, A.SALES_AMOUNT
+
+/*SELECT TO_CHAR(A.SALES_DATE, 'YYYY-MM-DD'), A.PRODUCT_ID, A.USER_ID, A.SALES_AMOUNT
 FROM
 (SELECT USER_ID, PRODUCT_ID, SALES_AMOUNT, SALES_DATE
     FROM ONLINE_SALE
@@ -10,4 +11,27 @@ FROM
     SELECT NULL AS USER_ID, PRODUCT_ID, SALES_AMOUNT, SALES_DATE
     FROM OFFLINE_SALE
     WHERE TO_CHAR(SALES_DATE, 'MM') = '03') A
-ORDER BY SALES_DATE, PRODUCT_ID, USER_ID
+ORDER BY SALES_DATE, PRODUCT_ID, USER_ID*/
+
+SELECT TO_CHAR(sales_date, 'YYYY-MM-DD') AS sales_date, product_id, user_id, sales_amount
+FROM online_sale
+WHERE TO_CHAR(sales_date, 'YYYY-MM') = '2022-03'
+
+UNION
+
+SELECT TO_CHAR(sales_date, 'YYYY-MM-DD') AS sales_date, product_id, NULL as user_id, sales_amount
+FROM offline_sale
+WHERE TO_CHAR(sales_date, 'YYYY-MM') = '2022-03'
+ORDER BY sales_date, product_id, user_id;
+
+
+
+
+
+
+
+
+
+
+
+
